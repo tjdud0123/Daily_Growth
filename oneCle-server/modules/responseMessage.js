8f165726f7267de95cadb3b39fdce0a66f9d0e1e
Um seoyoung <tjdud0123@naver.com>
2020-11-16 00:37:41 +0900

    # s - 필요 모듈 셋팅

---
+++ oneCle-server/modules/responseMessage.js
@@ -0,0 +1,56 @@
+module.exports = {
+  NULL_VALUE: '필요한 값이 없습니다',
+
+  OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
+
+  TOKEN_NULL_VALUE: '토큰 값이 없습니다.',
+
+  TOKEN_NOT_VALID_VALUE: '토큰이 유효하지 않습니다.',
+
+  LID_NULL_VALUE: 'lid값이 없습니다.',
+
+  DID_NULL_VALUE: 'did값이 없습니다.',
+
+  CID_NULL_VALUE: 'cid값이 없습니다.',
+
+  //수업관리, 수업일지
+
+  NO_LECTURE: '해당 수업이 없습니다. lid를 확인해 주세요.',
+
+  NO_DIARY: '해당 수업일지가 없습니다. did를 확인해 주세요.',
+
+  NO_CLASS: '해당 일정이 없습니다. cid를 확인해 주세요.',
+
+  //수업관리 , 수업일지, 알림 [토글 수업목록 조회]
+
+  CHECK_LECTURE_NAME_LIST_SUCCESS: '수업이름 목록 조회 성공',
+
+  // 회원가입
+
+  CREATED_USER: '회원 가입 성공',
+
+  DELETE_USER: '회원 탈퇴 성공',
+
+  ALREADY_ID: '존재하는 email 입니다.',
+
+  // 로그인
+
+  LOGIN_SUCCESS: '로그인 성공',
+
+  LOGIN_FAIL: '로그인 실패',
+
+  NO_USER: '존재하지 않는 유저입니다.',
+
+  MISS_MATCH_PW: '비밀번호가 일치하지 않습니다.',
+
+  // 간편 프로필 조회
+
+  READ_PROFILE_SUCCESS: '간편 프로필 조회 성공',
+
+  // 간편 프로필 수정
+
+  CHANGE_PROFILE_SUCCESS: '프로필 수정 성공',
+
+  //DB ERROR
+  DB_ERROR: '데이터베이스 오류',
+};
