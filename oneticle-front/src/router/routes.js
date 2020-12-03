b93baaef7f3b46ce3d5bbb5c3f0b02c7f22ce4ff
Um seoyoung <tjdud0123@naver.com>
2020-12-04 02:42:19 +0900

    # f - 페이지 라우트 설정

--- oneticle-front/src/router/routes.js
+++ oneticle-front/src/router/routes.js
@@ -1,20 +1,10 @@
-const Home = { template: '<div>Home</div>' };
-const NotFound = { template: '<div>Not Found</div>' };
 export default [
-  { path: '/', component: Home },
-  {
-    path: '/posts',
-    component: {
-      template: '<div>Posts <br/><router-view></router-view></div>',
-    },
-    // 중첩된 라우트는 children 속성으로 하위 라우트를 정의할 수 있다.
-    children: [
-      {
-        path: 'new',
-        component: () => import('../components/HelloWorld.vue'),
-      },
-      { path: ':id', component: { template: '<div>Post Detail</div>' } },
-    ],
-  },
-  { path: '*', component: NotFound },
+  { path: '/', component: () => import('../pages/main') },
+  { path: '/login', component: () => import('../pages/login') },
+  { path: '/signup', component: () => import('../pages/signup') },
+  { path: '/home', component: () => import('../pages/home') },
+  { path: '/articleView', component: () => import('../pages/articleView') },
+  { path: '/history', component: () => import('../pages/history') },
+  { path: '/myPage', component: () => import('../pages/myPage') },
+  { path: '*', component: { template: '<div>Not Found</div>' } },
 ];
