68a6f3926d62f6bc175d73aef64513cc0ad699cc
Um seoyoung <tjdud0123@naver.com>
2020-12-20 16:18:01 +0900

    #f- 홈화면 1차구현, moment로 날짜처리, 빈화면 구현

---
+++ oneticle-front/src/components/Balloon.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="balloon bg-success text-white text-nowrap py-3 px-5 mb-4">
+    {{ text }}
+    <div class="triangle"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      defualt: '',
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.balloon {
+  border-radius: 10px;
+  width: 80%;
+  min-width: 130px;
+  max-width: 570px;
+  position: relative;
+}
+
+.triangle {
+  position: absolute;
+  background-color: #08d50d;
+  text-align: left;
+  bottom: 0px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+.triangle:before,
+.triangle:after {
+  content: '';
+  position: absolute;
+  background-color: inherit;
+}
+.triangle,
+.triangle:before,
+.triangle:after {
+  width: 20px;
+  height: 20px;
+  border-top-right-radius: 40%;
+}
+
+.triangle {
+  transform: rotate(0deg) skewX(-30deg) scale(1, 0.866);
+}
+.triangle:before {
+  transform: rotate(-135deg) skewX(-45deg) scale(1.414, 0.707)
+    translate(0, -50%);
+}
+.triangle:after {
+  transform: rotate(135deg) skewY(-45deg) scale(0.707, 1.414) translate(50%);
+}
+</style>
--- oneticle-front/src/pages/home/Home.vue
+++ oneticle-front/src/pages/home/Home.vue
@@ -1,14 +1,69 @@
 <template>
-  <div>
-    <h1>Home</h1>
+  <div class="container mt-5 py-2">
+    <h1 class="font-weight-bold text-left my-3">
+      {{ todayDate }}<br />오늘의 글
+    </h1>
+    <div
+      id="noArticle"
+      class="d-flex flex-column align-items-center"
+      v-show="!noArticles"
+    >
+      <balloon text="아직 글이 도착하지 않았어요!"></balloon>
+      <img id="no-article-img" src="../../assets/home/notyet.png" alt="" />
+    </div>
+    <b-loading
+      :is-full-page="true"
+      v-model="isLoading"
+      :can-cancel="false"
+    ></b-loading>
   </div>
 </template>
 
 <script>
+import { getTodayArticlesApi } from '../../api/articleApi';
+import moment from 'moment';
+import 'moment/locale/ko';
+import Balloon from '../../components/Balloon.vue';
+
 export default {
   name: 'Home',
   props: {},
-  components: {},
+  data() {
+    return {
+      isLoading: false,
+      todayArticles: [],
+    };
+  },
+  components: { Balloon },
+  mounted() {
+    this.getTodayArticles();
+  },
+  methods: {
+    async getTodayArticles() {
+      this.isLoading = true;
+      const response = await getTodayArticlesApi();
+      // 토큰 만료
+      if (response.status === 401) {
+        localStorage.setItem('token', null);
+        this.$router.push({ path: '/' });
+        return;
+      }
+      this.todayArticles = response.data;
+      console.log(this.todayArticles);
+      this.isLoading = false;
+    },
+  },
+  computed: {
+    noArticles() {
+      return this.todayArticles.length === 0;
+    },
+    todayDate() {
+      const dateArray = moment()
+        .format('LLLL')
+        .split(' ');
+      return `${dateArray[1]} ${dateArray[2]}`;
+    },
+  },
 };
 </script>
 
